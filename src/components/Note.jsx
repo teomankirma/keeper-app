644a673e7a8b0def76f77de643930537372f3270
teomankirma <83593653+teomankirma@users.noreply.github.com>
2023-01-20 16:38:16 +0300

    feat: part 2 challenge solution

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,11 @@
-function Note() {
+import React from "react";
+import notes from "../notes";
+
+function Note(props) {
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
